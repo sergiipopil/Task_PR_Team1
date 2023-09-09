0ce2d86a17eced962580532a8e51faf133b3d599
Serhii Popil <sergii.popil@gmail.com>
2023-09-09 11:40:21 +0300

    add Class PersonalInfo with method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using Task_PR_Team1.Classes;
+
+Console.Write("Enter your name: ");
+var name = Console.ReadLine();
+
+Console.Write("Enter your surname: ");
+var surname = Console.ReadLine();
+
+PersonalInfo personalInfo = new PersonalInfo(name, surname);
+var info = personalInfo.GetUserInfo();
+Console.WriteLine(info);
