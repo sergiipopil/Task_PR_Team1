8566f184c5b0e96ab888daa2a70a8b042f7656a2
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-09-12 08:49:35 +0300

    Added new files

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Task_PR_Team1.Classes;
+using Task_PR_Team1.marks_folder;
 
 Console.Write("Enter your name: ");
 var name = Console.ReadLine();
@@ -21,5 +22,11 @@ Countries favorite = new Countries(favorCountry, capitolCountry);
 favorite.GiveCountry(favorCountry, capitolCountry);
 
 Console.WriteLine("Finish home task with PR");
+
+int x = 12321;
+var palid = new Palindrome();
+
+bool isPalindrome = palid.IsPalindrome(x);
+Console.WriteLine(isPalindrome);
 
 Console.WriteLine("It's Mark's comment");
